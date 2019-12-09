38578add3db89870a98c907e7655f0120692075b
Кевин Константинов <konstantinov.kevin@mail.ru>
2019-12-09 17:12:43 +0200

    Добавил страницу для пользователей на которой отображаються все имена и на которой можно добовлять новые имена, а также добавил админ панель в которой отображаются имена и их можно добовлять, изменять или удалять, а в админ панели и на странице пользователся присутствует сортировка

--- Autocompleter/Controllers/HomeController.cs
+++ Autocompleter/Controllers/HomeController.cs
@@ -1,23 +1,78 @@
-﻿using System;
+﻿using Autocompleter.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Autocompleter.Controllers
 {
     public class HomeController : Controller
     {
-        public string Index()
+        UserContext db = new UserContext();
+        ClientsContext dbU = new ClientsContext();
+        public int getUserId()
         {
-            string result = "Вы не авторизованы";
-            if (User.Identity.IsAuthenticated)
+            int userId = 0;
+            foreach (var u in db.Users)
             {
-                result = "Ваш логин: " + User.Identity.Name;
+                if (u.Email == User.Identity.Name)
+                {
+                    userId = u.Id;
+                    break;
+                }
             }
-            return result;
+            return userId;
         }
-        [Authorize(Roles ="admin")]
+        public int getUserRole()
+        {
+            int roleid = 0;
+            foreach (var u in db.Users)
+            {
+                if (u.Email == User.Identity.Name)
+                {
+                    roleid = u.RoleId;
+                    break;
+                }
+            }
+            return roleid;
+        }
+        public ActionResult Index(string sortClient, string searchString)
+        {
+            ClientsContext db = new ClientsContext();
+            ViewBag.roleid = getUserRole();//Добавить в каждый Action Result для админа
+
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortClient) ? "Name desc" : "";
+            var clients = from s in db.People
+                          select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                clients = clients.Where(s => s.Namee.ToUpper().Contains(searchString.ToUpper())
+                                       || s.TrueName.ToUpper().Contains(searchString.ToUpper()));
+            }
+            switch (sortClient)
+            {
+                case "Name desc":
+                    clients = clients.OrderByDescending(s => s.Namee);
+                    break;
+                default:
+                    clients = clients.OrderBy(s => s.Namee);
+                    break;
+            }
+
+
+            ViewBag.client = clients;
+            return View(clients.ToList());
+
+
+
+
+        }
+
+
+        [Authorize(Roles = "admin")]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -25,11 +80,84 @@ namespace Autocompleter.Controllers
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Admin()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.roleid = getUserRole();
+            ClientsContext db = new ClientsContext();
+            var clients = from x in db.People
+                          select x;
+            ViewBag.client = db.People;
+            return View(clients.ToList());
 
+
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            Session.Abandon(); // it will clear the session at the end of request
+            return RedirectToAction("index", "Home");
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Create(People people)
+        {
+            dbU.People.Add(people);
+            dbU.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            ViewBag.roleid = getUserRole();
+
+            People b = dbU.People.Find(id);
+            if (b == null)
+            {
+                return HttpNotFound();
+            }
+            return View(b);
+        }
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            People b = dbU.People.Find(id);
+            if (b == null)
+            {
+                return HttpNotFound();
+            }
+            dbU.People.Remove(b);
+            dbU.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+
+        public ActionResult Edit(int? id)
+        {
+            ViewBag.roleid = getUserRole();
+
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            People client = dbU.People.Find(id);
+            if (client != null)
+            {
+                return View(client);
+            }
+            return HttpNotFound();
+        }
+        [HttpPost]
+
+        public ActionResult Edit(People client)
+        {
+            db.Entry(client).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ Autocompleter/Models/ClientContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Autocompleter.Models
+{
+    public class ClientsContext : DbContext
+    {
+        public DbSet<People> People { get; set; }
+
+        public ClientsContext() : base("Clients")
+        {
+            if (!Database.Exists())
+                Database.Create();
+
+            SaveChanges();
+        }
+
+
+    }
+}
